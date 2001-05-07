503aa2fc4d733e55e44fa9406a16ad03c3d1e02d
Lee ♫ Goddard <lgoddard@cpan.org>
2001-05-07 09:48:27 +0000

    Import of LGODDARD/Poetry-Aum-1 from CPAN.
    gitpan-cpan-distribution: Poetry-Aum
    gitpan-cpan-version:      1
    gitpan-cpan-path:         LGODDARD/Poetry-Aum-1.tar.gz
    gitpan-cpan-author:       LGODDARD
    gitpan-cpan-maturity:     released

--- Aum.pm
+++ Aum.pm
@@ -1,26 +1,65 @@
+=head1 NAME
+
+Poetry::Aum
+
+=head1 DESCRIPTION
+
+The sound the breath makes when focused upon.
+
+=head1 SYNOPSIS
+
+	use Poetry::Aum
+
+=head1 DEPENDENCIES
+
+	Abstract::Concept
+	Abstract::Entity
+	Abstract::Interest
+
+=head1 CONSTRUCTOR METHOD new
+
+If you feel you need documentation for this module, please consult
+an experienced Perl Monger.
+
+=cut
+
 package Poetry::Aum;
 use strict;
 use warnings;
-our $VERSION = 0;
+our $VERSION = 1;
 
 sub new {
 	my $self = new Abstract::Entity(rand);
-	my $ego  = new Abstract::Concept;
-	my $goal = new Abstract::Interest;
-	our $philosophy;
+	$self->{ego}  = new Abstract::Concept;
+	$self->{goal} = new Abstract::Interest;
+	bless $self;	# Intentionally classless
+
+	my $philosophy;
 
 	DARK_NIGHT:
 	while (defined $goal){
 		$philosophy = <STDIN>;
 		study $philosophy;
-		if ($ego->understands($philosophy)){
+		if ($self->understands($philosophy)){
 			undef $goal; # implies last DARK_NIGHT;
 		}
 	}
 
 	$self->{$philosophy} = localtime;
-	bless $self;	# Intentionally classless
 	return;			# Does this hand over my $self?
 }
 
 1;
+
+
+=head1 AUTHOR
+
+Lee Goddard (LGoddard@CPAN.org) 07/05/2001 10:37
+
+=head1 COPYRIGHT
+
+Copyright 2001 Lee Goddard.
+
+This library is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
